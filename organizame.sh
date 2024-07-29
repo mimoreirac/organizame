a8196e9e897830f2b5df6ad04947649e33a45b08
Martin Moreira <172557112+mimoreirac@users.noreply.github.com>
2024-07-28 19:03:10 -0500

    Agrega directorio por defecto y mensajes para el usuario

--- organizame.sh
+++ organizame.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
+# Modo de uso:
+# ./organizame.sh /ruta/de/un/directorio
+
 # Directorios
-DIR_BASE="$HOME/Downloads/"
+DIR_POR_DEFECTO="$HOME/Downloads/"
+DIR_BASE="${1:-$DIR_POR_DEFECTO}" # Si el usuario pone un directorio, opera en ese. Caso contrario opera dentro del directorio Descargas.
 DIR_IMG="$HOME/Pictures/"
 DIR_DOCS="$HOME/Documents/"
 DIR_VIDEOS="$HOME/Videos/"
 
-# Crear directorios si no existen
+# Chequea que el directorio base exista
+if [ ! -d "$DIR_BASE" ]; then
+	echo "Error: El directorio $DIR_BASE no existe."
+	exit 1
+fi
+
+# Crear directorios de destino si no existen
 mkdir -p "$DIR_DOCS" "$DIR_IMG" "$DIR_VIDEOS"
 
 # Mover archivos
+echo "Organizando archivos en el directorio $DIR_BASE ..."
+
 for file in "$DIR_BASE"/*; do
 	if [ -f "$file" ]; then
 		case "${file,,}" in
@@ -19,9 +31,11 @@ for file in "$DIR_BASE"/*; do
 		*.pdf | *.doc | *.docx | *.txt)
 			mv "$file" "$DIR_DOCS"
 			;;
-		*.mp4 | *.avi | *.mkv)
+		*.mp4 | *.avi | *.mkv | *.webm)
 			mv "$file" "$DIR_VIDEOS"
 			;;
 		esac
 	fi
 done
+
+echo "Tus archivos han sido organizados!"
